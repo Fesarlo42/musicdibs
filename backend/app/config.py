57d905ebeb03ec2c65d641330438bb0f4daf3968
Fesarlo <fesarlo@gmail.com>
2025-04-15 22:03:25 +0200

    add: functional files

---
+++ backend/app/config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv() # load the variables
+
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASS = os.getenv("DATABASE_PASS")
+DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "musicdibs")
+
+DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASS}@{DATABASE_HOST}/{DATABASE_NAME}"
