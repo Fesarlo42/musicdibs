59d60b7aeca9713678298bee213c2d1ce6eebcfc
Fesarlo <fesarlo@gmail.com>
2025-04-26 12:24:22 +0200

    fix: responsive in verification

--- frontend/src/views/Verify.vue
+++ frontend/src/views/Verify.vue
@@ -2,7 +2,7 @@
   <div class="p-4">
     <h1 class="text-2xl font-bold">Verificar obra</h1>
     <section class="my-10 py-10">
-      <div class="musicdibs-card w-3/5 bg-base-100">
+      <div class="musicdibs-card w-full bg-base-100 sm:w-3/5">
         <div class="card-body" v-if="registrationsStore.verification">
           <div v-if="registrationsStore.verification.status == 'failure'">
             <h2 class="card-title">Lo sentimos</h2>
