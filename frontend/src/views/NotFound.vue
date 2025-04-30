fc5c5743259cfe54a2415c0280a2db1e214874fa
Fesarlo <fesarlo@gmail.com>
2025-04-30 21:54:20 +0200

    add: not found and unauthorized

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="p-4">
-    <h1 class="text-2xl font-bold">NotFound</h1>
-    <div class="mt-4">
-      <button class="btn btn-primary">Primary Button</button>
-      <button class="btn btn-secondary ml-2">Secondary Button</button>
-      <button class="btn btn-accent ml-2">Accent Button</button>
-    </div>
-    <div class="mt-4">
-      <div class="musicdibs-card secondary w-96 bg-base-100">
-        <div class="card-body">
-          <h2 class="card-title">Card Title</h2>
-          <p>
-            If this card has the synthwave theme colors, the theme is working!
-          </p>
-        </div>
+    <div class="flex items-center justify-center">
+      <div class="size-sm m-10">
+        <img
+          class="w-[90%]"
+          src="../assets/images/summer_dj.png"
+          alt="Not found"
+        />
+      </div>
+      <div class="flex flex-grow-0 flex-col items-center justify-center">
+        <h2 class="text-center text-4xl font-bold">Ups, no hay nada aquí...</h2>
+        <button class="btn btn-primary btn-wide m-10">
+          <router-link>Volver al inicio</router-link>
+        </button>
       </div>
     </div>
   </div>
