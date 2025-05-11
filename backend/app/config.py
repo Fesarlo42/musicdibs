5a9a1dad57fc98e227f58bdd836bc927b2ad4569
Fesarlo <fesarlo@gmail.com>
2025-05-11 18:38:50 +0200

    fix: change database host to use unix socket

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,4 +8,8 @@ DATABASE_PASS = os.getenv("DATABASE_PASS")
 DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "musicdibs")
 
-DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASS}@{DATABASE_HOST}/{DATABASE_NAME}"
+# DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASS}@{DATABASE_HOST}/{DATABASE_NAME}"
+DATABASE_URL = (
+    f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASS}@/{DATABASE_NAME}"
+    f"?unix_socket={DATABASE_HOST}"
+)
