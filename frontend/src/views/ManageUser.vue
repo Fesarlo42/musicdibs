046a0aee60773e6e6d5196e33cb1ecb8e9cd2f5d
Fesarlo <fesarlo@gmail.com>
2025-05-16 17:57:23 +0200

    add: change user role from admin panel

--- frontend/src/views/ManageUser.vue
+++ frontend/src/views/ManageUser.vue
@@ -59,6 +59,12 @@
                 </span>
               </p>
               <p class="my-2">
+                <span class="pr-2 font-semibold">Perfil:</span>
+                <span>
+                  {{ userData.role }}
+                </span>
+              </p>
+              <p class="my-2">
                 <span class="pr-2 font-semibold">Creditos:</span>
                 <span>
                   {{ userBalance }}
@@ -90,6 +96,39 @@
                   />
                 </svg>
                 Gestionar creditos
+              </button>
+              <button
+                class="btn btn-primary btn-link btn-sm w-auto self-start border-0"
+                @click="handleChangeRole"
+              >
+                <svg
+                  v-if="userData.role === 'user'"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    d="M5.25 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM2.25 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM18.75 7.5a.75.75 0 0 0-1.5 0v2.25H15a.75.75 0 0 0 0 1.5h2.25v2.25a.75.75 0 0 0 1.5 0v-2.25H21a.75.75 0 0 0 0-1.5h-2.25V7.5Z"
+                  />
+                </svg>
+                <svg
+                  v-else
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    d="M10.375 2.25a4.125 4.125 0 1 0 0 8.25 4.125 4.125 0 0 0 0-8.25ZM10.375 12a7.125 7.125 0 0 0-7.124 7.247.75.75 0 0 0 .363.63 13.067 13.067 0 0 0 6.761 1.873c2.472 0 4.786-.684 6.76-1.873a.75.75 0 0 0 .364-.63l.001-.12v-.002A7.125 7.125 0 0 0 10.375 12ZM16 9.75a.75.75 0 0 0 0 1.5h6a.75.75 0 0 0 0-1.5h-6Z"
+                  />
+                </svg>
+
+                {{
+                  userData.role === "user"
+                    ? "Hacer administrador"
+                    : "Bajar de categoría a usuario"
+                }}
               </button>
               <button
                 class="btn btn-primary btn-link btn-sm w-auto self-start border-0"
@@ -170,6 +209,14 @@ onMounted(async () => {
   }
 });
 
+const handleChangeRole = async () => {
+  const newRole = userData.value.role === "user" ? "admin" : "user";
+
+  userData.value = await usersStore.updateUser(userData.value.id, {
+    role: newRole,
+  });
+};
+
 const handleDeleteUser = async () => {
   let confirm = window.confirm(
     "¿Estás seguro de que quieres eliminar ese usuario? Esta acción no se puede deshacer.",
@@ -181,7 +228,7 @@ const handleDeleteUser = async () => {
 };
 
 const handleModal = () => {
-  const modal = document.getElementById("credits_modal");
+  const credits_modal = document.getElementById("credits_modal");
   credits_modal.showModal();
 };
 </script>
